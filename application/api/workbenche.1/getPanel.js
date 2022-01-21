a3e3aedd22a7b6174e97fed276c601f9f77b3814
Dmitry Fokin <dkoder.dvl@gmail.com>
2022-01-21 08:20:20 +0300

    update api workbenche

--- application/api/workbenche.1/getPanel.js
+++ application/api/workbenche.1/getPanel.js
@@ -1,7 +1,6 @@
 ({
   access: 'public',
   method: async () => {
-    console.debug({a: await lib.workbenche.getPanel.method()});
     return { menu: await lib.workbenche.getPanel.method() };
   },
 });
